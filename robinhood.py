8decb617f2b87f8d14ccdbe992303ebe1ec04bc5
Cthax12 <Cthax12@gmail.com>
2021-12-02 14:15:36 -0600

    Created excel sheet of option trades and entry prices

--- robinhood.py
+++ robinhood.py
@@ -2,6 +2,10 @@ import robin_stocks.robinhood as r
 from pyrh import Robinhood
 import pandas as pd
 import csv
+from robin_stocks.robinhood.export import export_completed_option_orders
+import xlsxwriter as xl
+
+
 
 email = input("Email: ")
 password = input("Password: ")
@@ -11,10 +15,11 @@ login = r.login(email, password)
 # Query your positions
 allPositions = r.get_all_option_positions()
 optionNames = []
-print(allPositions[0])
-for pos in allPositions:
+entryPrices = []
+print(len(allPositions))
+for i in range(1, len(allPositions), 2):
   
-  option = r.get_option_instrument_data_by_id(pos["option_id"])
+  option = r.get_option_instrument_data_by_id(allPositions[i]["option_id"])
   ticker = option["chain_symbol"]
   strike = option["strike_price"]
   callOrPut = option["type"]
@@ -22,8 +27,16 @@ for pos in allPositions:
 
   optionNames.append("{} ${} {} {}".format(ticker, strike, callOrPut, date))
 
-entryPrices = []
+  entryPrice = allPositions[i]["average_price"]
+  entryPrices.append(entryPrice)
+
+print(len(optionNames))
+df = pd.DataFrame({"Option Name" : optionNames, "Entry Price" : entryPrices})
+writer = pd.ExcelWriter("OptionTrades.xlsx", engine = 'xlsxwriter')
+df.to_excel(writer, sheet_name="Sheet1")
+writer.save()
 
 closingPrices = []
 
 r.logout()
+
