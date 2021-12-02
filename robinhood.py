9f71470ebd2ee2d84e0094b18436d57ad125bb34
Cthax12 <Cthax12@gmail.com>
2021-12-02 14:55:21 -0600

    Added pie chart with put/call freq.

--- robinhood.py
+++ robinhood.py
@@ -16,6 +16,9 @@ login = r.login(email, password)
 allPositions = r.get_all_option_positions()
 optionNames = []
 entryPrices = []
+calls = 0
+puts = 0
+
 print(len(allPositions))
 for i in range(1, len(allPositions), 2):
   
@@ -28,12 +31,45 @@ for i in range(1, len(allPositions), 2):
   optionNames.append("{} ${} {} {}".format(ticker, strike, callOrPut, date))
 
   entryPrice = allPositions[i]["average_price"]
-  entryPrices.append(entryPrice)
+  entryPrices.append("$" + entryPrice)
+
+  if "call" in callOrPut:
+    calls +=1
+  else:
+    puts +=1
 
 print(len(optionNames))
 df = pd.DataFrame({"Option Name" : optionNames, "Entry Price" : entryPrices})
 writer = pd.ExcelWriter("OptionTrades.xlsx", engine = 'xlsxwriter')
 df.to_excel(writer, sheet_name="Sheet1")
+
+workbook = writer.book
+worksheet = writer.sheets["Sheet1"]
+chart1 = workbook.add_chart({'type': 'pie'})
+
+# Configure the series. Note the use of the list syntax to define ranges:
+worksheet.write_string("E2", "Calls")
+worksheet.write_number("E3", calls)
+worksheet.write_string("F2", "Puts")
+worksheet.write_number("F3", puts)
+
+
+chart1.add_series({
+    'name':       'Pie sales data',
+    'data_labels': {'value': True, 'category': True},
+    'categories': ["Sheet1", 1, 4, 1, 5],
+    'values':     ["Sheet1", 2, 4, 2, 5]
+})
+
+# Add a title.
+chart1.set_title({'name': 'Call vs Put Frequency'})
+
+# Set an Excel chart style. Colors with white outline and shadow.
+chart1.set_style(10)
+
+# Insert the chart into the worksheet (with an offset).
+worksheet.insert_chart('E8', chart1, {'x_offset': 25, 'y_offset': 10})
+
 writer.save()
 
 closingPrices = []
